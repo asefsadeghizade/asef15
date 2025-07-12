0d9153c9fa758dad8b946d4674e20a9f250ec3f9
asefsadeghizade <asefsadeghi@gmail.com>
2025-07-12 19:46:06 +0330

    Added the requirements for db connection.

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,8 +27,17 @@ SECRET_KEY = 'django-insecure-)=*qwkzjbhq@-se@w+l0z)t@e5(tx13%v5-mkqx+t9uhw@2d=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.liara.run',  # This will allow all subdomains of liara.run
+    'asef15.liara.run',
+]
 
+
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    'https://asef15.liara.run',
+    'http://asef15.liara.run',
+]
 
 # Application definition
 
@@ -75,12 +85,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://root:vkoCPvy3OSyZ9xE57sqF0mZU@certificate-db:5432/postgres'
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
